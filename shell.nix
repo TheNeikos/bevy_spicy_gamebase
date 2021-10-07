031fd17a1893b2d8e0d2219eb5223d4abcd50fc6
Marcel Müller <neikos@neikos.email>
2021-10-07 13:29:38 +0200

    Add neuron to nix-shell

--- shell.nix
+++ shell.nix
@@ -1,31 +1,41 @@
-with import <nixpkgs>{};
+with import <nixpkgs> { };
 
 stdenv.mkDerivation {
-    name = "bevy-spicy-gamebase";
+  name = "bevy-spicy-gamebase";
 
-    buildInputs = [
-        gcc
-        pkgconfig 
-        x11 
-        xorg.libXcursor 
-        xorg.libXrandr 
-        xorg.libXi 
-        vulkan-tools
-        vulkan-headers 
-        vulkan-loader 
-        vulkan-validation-layers 
+  buildInputs = [
+    gcc
+    pkgconfig
+    x11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    vulkan-tools
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+    alsaLib
+    udev
+    python3
+    (
+      let
+        neuronRev = "e3f3349e42dab59384a705001209e0e8741c4af4";
+        neuronSrc = builtins.fetchTarball "https://github.com/srid/neuron/archive/${neuronRev}.tar.gz";
+        neuronPkg = import neuronSrc;
+      in
+      neuronPkg.default
+    )
+  ];
+
+
+
+  shellHook =
+    let
+      dlopen-libs = [
+        vulkan-loader
         alsaLib
         udev
-        python3
-    ];
-
-    
-
-    shellHook = let 
-        dlopen-libs = [
-            vulkan-loader
-            alsaLib
-            udev
-        ];
-        in "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib.makeLibraryPath dlopen-libs}";
+      ];
+    in
+    "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib.makeLibraryPath dlopen-libs}";
 }
